30123a53ca89101d39159e49a9b9a526fa311f6a
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-09-10 22:59:42 +0530

    Blood Donation part Completed

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("clicked", " ");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " clicked";
+}
