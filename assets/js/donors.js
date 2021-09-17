dad767f61d3d0947fc49a8cee23d70acada4bd42
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-09-17 12:53:22 +0530

    Added icons

--- assets/js/donors.js
+++ assets/js/donors.js
@@ -4,6 +4,12 @@ function display(evt, tabname) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+    if (tabname == "organ") {
+        document.getElementById("mapbtn").style.display = "none";
+    }
+    else {
+        document.getElementById("mapbtn").style.display = "block";
+    }
     buttons = document.getElementsByClassName('tabs');
     for (i = 0; i < buttons.length; i++) {
         buttons[i].className = buttons[i].className.replace("btn-danger", " btn-outline-danger ");
